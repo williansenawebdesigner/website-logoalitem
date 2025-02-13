3cd670952aefbf36422ca147210012141e027e2d
Renato Fiorezze Silva <renato.fsilva@parceiro.mrv.com.br>
2025-02-13 18:32:59 -0300

    Incluindo gtag no site e configurando conversões

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,27 +6,41 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Logo Ali Tem - Simplificando a Vida do Transportador",
-  description: "Tudo o que você precisa em um só lugar: desde cálculo de frete e assessoria jurídica até reboques e parcerias que garantam mais eficiência e segurança.",
+  description:
+    "Tudo o que você precisa em um só lugar: desde cálculo de frete e assessoria jurídica até reboques e parcerias que garantam mais eficiência e segurança.",
   icons: [
-    { rel: 'icon', url: '/favicon.png' },
-    { rel: 'apple-touch-icon', url: '/favicon.png' },
-    { rel: 'shortcut icon', url: '/favicon.png' }
-  ]
+    { rel: "icon", url: "/favicon.png" },
+    { rel: "apple-touch-icon", url: "/favicon.png" },
+    { rel: "shortcut icon", url: "/favicon.png" },
+  ],
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16866351206"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16866351206');
+            `,
+          }}
+        />
         <link rel="icon" href="/favicon.png" />
       </head>
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
